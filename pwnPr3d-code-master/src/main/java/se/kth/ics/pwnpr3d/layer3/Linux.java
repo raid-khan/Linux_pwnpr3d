68c1f795c7b2942fd730c6d36ff28e7b3d915e30
raid-khan <khanraid@hotmail.com>
2016-08-09 21:48:26 +0200

    Update Linux.java

--- pwnPr3d-code-master/src/main/java/se/kth/ics/pwnpr3d/layer3/Linux.java
+++ pwnPr3d-code-master/src/main/java/se/kth/ics/pwnpr3d/layer3/Linux.java
@@ -1,11 +1,13 @@
 package se.kth.ics.pwnpr3d.layer3;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import se.kth.ics.pwnpr3d.datatypes.PrivilegeType;
 import se.kth.ics.pwnpr3d.datatypes.ProtocolType;
 import se.kth.ics.pwnpr3d.layer1.Account;
 import se.kth.ics.pwnpr3d.layer1.Data;
 import se.kth.ics.pwnpr3d.layer2.computer.Computer;
-import se.kth.ics.pwnpr3d.layer2.computer.HardwareComputer;
 import se.kth.ics.pwnpr3d.layer2.network.EthernetSwitch;
 import se.kth.ics.pwnpr3d.layer2.network.Firewall;
 import se.kth.ics.pwnpr3d.layer2.network.Router;
@@ -14,49 +16,178 @@ import se.kth.ics.pwnpr3d.layer2.network.protocolImplementations.ARPImplementati
 import se.kth.ics.pwnpr3d.layer2.network.protocolImplementations.EthernetImplementation;
 import se.kth.ics.pwnpr3d.layer2.network.protocolImplementations.IPImplementation;
 import se.kth.ics.pwnpr3d.layer2.software.Application;
-import se.kth.ics.pwnpr3d.layer2.software.DatabaseServer;
 import se.kth.ics.pwnpr3d.layer2.software.NetworkedApplication;
 import se.kth.ics.pwnpr3d.layer2.software.OperatingSystem;
+import se.kth.ics.pwnpr3d.layer2.software.WebApplication;
+import se.kth.ics.pwnpr3d.layer2.software.WebServer;
 
 public class Linux extends OperatingSystem {
 
-    private DatabaseServer dataServer;
     private NetworkedApplication ssh;
-    private Account userAccount;
     private EthernetSwitch ethernetSwitch;
     private Router ipRouter;
     private Firewall firewall;
-    private Application application;
+    private WebServer webServer;
+    private WebApplication config;
+    private WebApplication test;
+    private WebApplication wheaterStyle;
+    private WebApplication index;
 
     public Linux(String name, Computer computer) {
         super(name, computer);
         
-        userAccount = super.newUserAccount("USER_ACCOUNT", PrivilegeType.User);
+        //we have two example accounts
+        Account alice = super.newUserAccount("Alice", PrivilegeType.User);
+        Account bob = super.newUserAccount("Bob", PrivilegeType.User);
+
+        //you could access the Linux machine through ssh
         ssh = this.newNetworkedApplication("SSH", PrivilegeType.User, ProtocolType.TCP, false, false); 
 
-        dataServer = this.newDatabaseServer("DataBase", PrivilegeType.Administrator, ProtocolType.TCP, false, false);
-		Data archive = new Data("Archive", true);
-		dataServer.addOwnedData(archive);
-		Data contacts = new Data("Contacts", false);
-		dataServer.addOwnedData(contacts);
-		Data folders = new Data("Folders", true);
-		dataServer.addOwnedData(folders);
-		Data documents = new Data("Documents", true);
-		dataServer.addOwnedData(documents);
-		Data backup = new Data("Backup", true);
-		dataServer.addOwnedData(backup);
-		userAccount.addAuthorizedAccess(dataServer);
-		getAdministrator().addAuthorizedAccess(dataServer);
-
-		application = this.newApplication("Application", PrivilegeType.Administrator, false);
-		userAccount.addAuthorizedAccess(application);
+        //The user directory
+        Data usr = new Data("usr", false);
+        //the directories of the user
+		Set<Data> usrData = new HashSet<Data>();
+        usrData.add(new Data("games", false));
+        usrData.add(new Data("local", false));
+        usrData.add(new Data("include", false));
+        usrData.add(new Data("share", false));
+        usrData.add(new Data("lib", false));
+        usrData.add(new Data("sbin", false));
+        usrData.add(new Data("bin", false));
+        usrData.add(new Data("rhosts", false));
+        Data app = new Data("Applications", false);
+        usrData.add(app);
+        //applications used by the user
+        Application gnomeCalc = this.newApplication("Gnome-Calculator", PrivilegeType.User, false);
+        Application gnomeDesk = this.newApplication("Gnome-Desktop3", PrivilegeType.User, false);
+        app.addRequiredAgent(gnomeCalc);
+        app.addRequiredAgent(gnomeDesk);
+        usr.addBodyAll(usrData);
+        //administrator have access to the user data
+        getAdministrator().addAuthorizedReadWrite(usr);
+        
+        //the etc directory
+        Data etc = new Data("etc", false);
+		Set<Data> etcData = new HashSet<Data>();
+        etcData.add(new Data("ldap", false));
+        etcData.add(new Data("crontab", false));
+        etcData.add(new Data("init.d", false));
+        etcData.add(new Data("fonts", false));
+        etcData.add(new Data("rc.d", false));
+        etcData.add(new Data("calendar", false));
+        //the password file
+        Data password = new Data("password", true);
+        etcData.add(password);
+        //application that checks is a user have access to a resource 
+        Application accessList = this.newApplication("Access List", PrivilegeType.Administrator, false);
+        password.addRequiredAgent(accessList);
+        //application used to add ne users
+        Application addUser = this.newApplication("addUser", PrivilegeType.Administrator, false);
+        password.addRequiredAgent(accessList);
+        //the networks folder
+        Data networks = new Data("networks", false);
+        networks.addBody(new Data("interfaces", true));
+        etcData.add(networks);
+        //the folders of etc
+        Data netConfig = new Data("netconfig", false);
+        netConfig.addBody(new Data("FTP", false));
+        netConfig.addBody(new Data("Telnet", false));
+        netConfig.addBody(new Data("FTP", false));
+        netConfig.addBody(new Data("TCP/UDP", false));
+        netConfig.addBody(new Data("SendMail", false));
+        netConfig.addBody(new Data("ARP", false));
+        etcData.add(netConfig);
+        etc.addBodyAll(etcData);
+        //alice could create users 
+        alice.addAuthorizedReadWrite(password);
+        alice.addAuthorizedAccess(accessList);
+        alice.addAuthorizedAccess(addUser);
+        alice.addAuthorizedRead(etc);
+        //administrador have full access
+        getAdministrator().addAuthorizedReadWrite(password);
+        getAdministrator().addAuthorizedAccess(accessList);
+        getAdministrator().addAuthorizedAccess(addUser);
+        getAdministrator().addAuthorizedReadWrite(etc);
+        
+        //bin folder 
+		Data bin = new Data("bin", false);
+		Application grep = this.newApplication("grep", PrivilegeType.Administrator, false);
+		//admin and these two users have access to this folder
+		alice.addAuthorizedAccess(grep);
+		bob.addAuthorizedAccess(grep);
+		getAdministrator().addAuthorizedAccess(grep);
+		bin.addRequiredAgent(grep);
 		
+		//the application mount
+		Application mount = this.newApplication("mount", PrivilegeType.Administrator, false);
+		//only admin is allowed to mount volumes
+		getAdministrator().addAuthorizedAccess(mount);
+		bin.addRequiredAgent(mount);
+		addOwnedData(bin);
+		
+		//the sbin folder
+		Data sbin = new Data("sbin", false);
+		addOwnedData(sbin);
+		alice.addAuthorizedRead(sbin);
+		bob.addAuthorizedRead(sbin);
+		getAdministrator().addAuthorizedReadWrite(sbin);
+
+		//the mnt folder
+		Data mnt = new Data("mnt", false);
+		Set<Data> mntDevices = new HashSet<Data>();
+		mntDevices.add(new Data("cd-rom", false));
+		mntDevices.add(new Data("floppy", false));
+		mnt.addBodyAll(mntDevices);
+		addOwnedData(mnt);
+		alice.addAuthorizedRead(mnt);
+		bob.addAuthorizedRead(mnt);
+		getAdministrator().addAuthorizedReadWrite(mnt);
+		
+		//the var folder directories
+		Set<Data> varDirs = new HashSet<Data>();
+		Data html = new Data("html", false);
+		varDirs.add(html);
+		varDirs.add(new Data("cache", false));
+		varDirs.add(new Data("lib", false));
+		varDirs.add(new Data("tmp", false));
+		varDirs.add(new Data("logs", false));
+		varDirs.add(new Data("run", false));
+		addOwnedData(html);
+		alice.addAuthorizedRead(html);
+		bob.addAuthorizedRead(html);
+		super.getAdministrator().addAuthorizedReadWrite(html);
+		
+		//the var folder
+		Data var = new Data("var", false);
+		var.addBodyAll(varDirs);
+		addOwnedData(var);
+		alice.addAuthorizedRead(var);
+		bob.addAuthorizedRead(var);
+		super.getAdministrator().addAuthorizedReadWrite(var);
+		
+		//the webserver
+		webServer = this.newWebServer("WebServer", PrivilegeType.Guest, ProtocolType.TCP, false, true);
+		webServer.addOwnedData(html);
+		config = new WebApplication("config", webServer);
+		test = new WebApplication("test", webServer);
+		wheaterStyle =  new WebApplication("wheaterStyle", webServer);
+		index = new WebApplication("index", webServer);
+		super.getAdministrator().addAuthorizedAccess(webServer);
+		super.getAdministrator().addAuthorizedAccess(config);
+		super.getAdministrator().addAuthorizedAccess(test);
+		super.getAdministrator().addAuthorizedAccess(wheaterStyle);
+		super.getAdministrator().addAuthorizedAccess(index);
+
+		//the switch
 		ethernetSwitch = new EthernetSwitch("EthernetSwicth");
+		//the router
 		ipRouter = new Router("Router");
 		ipRouter.connect(this, ethernetSwitch);
+		//the firewall
 		firewall = new Firewall("Firewall");
 		firewall.connect(ipRouter, true);
 		ethernetSwitch.connect(firewall);
+		//the interface and the protocols
 		IPEthernetARPNetworkInterface ipNetIface = new IPEthernetARPNetworkInterface("IPFACE", computer, 0.3);
 		EthernetImplementation networkProtocols = new EthernetImplementation("NetworkProtocols", ipNetIface);
 		IPImplementation tcpIP = new IPImplementation("TCP/IP", ipNetIface);
