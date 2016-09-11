f1180a4da9054128013130a99bbc36a34c51418d
raid-khan <khanraid@hotmail.com>
2016-09-11 15:44:24 +0200

    improvised

--- pwnPr3d-code-master/src/test/java/se/kth/ics/pwnpr3d/functional/linux/Linux_PrivilegeScalationTest.java
+++ pwnPr3d-code-master/src/test/java/se/kth/ics/pwnpr3d/functional/linux/Linux_PrivilegeScalationTest.java
@@ -19,49 +19,48 @@ import se.kth.ics.pwnpr3d.util.TestSupport;
  * from an application or user.
  */
 
-/*
+ /*
  * In this test the attacker get access to the administrator account through
  * a compromised user account, spoofing the administrator and compromising the entire system.
  */
 
-/*
+ /*
  * The main problem faced here is to use a guest account to access the administrator
  * account. With this account the attacker spoof the administrator account to get administrator
  * privileges
  */
-
 public class Linux_PrivilegeScalationTest {
 
-	@Test
-	public void testPrivilegeScalation(){
-		//create a new computer
-		HardwareComputer computer = new HardwareComputer("LINUX_MACHINE_PRIVILEGE_TEST");
-		//create a new linux host
-		Linux linuxHost = new Linux("LINUX_HOST_PRIVILEGE_TEST", computer);
-		//create a new vulnerability 
-		Vulnerability privEscVuln = new Vulnerability("PRIVILEGE_VULNERABILITY", linuxHost, ImpactType.High);
-		//add the vulnerability to a guest user in the linux host
-		linuxHost.getGuest().addVulnerability(privEscVuln);
-		//add a spoofed identity to the guest user from the administrator
-		//scale the identity
-		privEscVuln.addSpoofedIdentity(linuxHost.getAdministrator());
-		//create a new attacker
-		Attacker attacker = new Attacker();
-		//attack the Linux host
-		attacker.addAttackPoint(linuxHost.getAccess());
-		//attack the compromised guest user
-		attacker.addAttackPoint(linuxHost.getGuest().getCompromise());
-		//perform the attack generating graphs
-		attacker.attackWithTTC();
-		//test the compromised administrator account
-		TestSupport.assertCompromised(linuxHost.getAdministrator().getCompromise());
-	}
+    @Test
+    public void testPrivilegeScalation() {
+        //create a new computer
+        HardwareComputer computer = new HardwareComputer("LINUX_MACHINE_PRIVILEGE_TEST");
+        //create a new linux host
+        Linux linuxHost = new Linux("LINUX_HOST_PRIVILEGE_TEST", computer);
+        //create a new vulnerability 
+        Vulnerability privEscVuln = new Vulnerability("PRIVILEGE_VULNERABILITY", linuxHost, ImpactType.High);
+        //add the vulnerability to a guest user in the linux host
+        linuxHost.bob.addVulnerability(privEscVuln);
+        //add a spoofed identity to the guest user from the administrator
+        //scale the identity
+        privEscVuln.addSpoofedIdentity(linuxHost.getAdministrator());
+        //create a new attacker
+        Attacker attacker = new Attacker();
+        //attack the Linux host
+        attacker.addAttackPoint(linuxHost.getAccess());
+        //attack the compromised guest user
+        attacker.addAttackPoint(linuxHost.bob.getCompromise());
+        //perform the attack generating graphs
+        attacker.attackWithTTC();
+        //test the compromised administrator account
+        TestSupport.assertCompromised(linuxHost.getAdministrator().getCompromise());
+    }
 
     @After
     public void emptySets() {
-    	//clear the tests data after the tests executes
-       Asset.clearAllAssets();
-       AttackStep.clearAllAttackSteps();
+        //clear the tests data after the tests executes
+        Asset.clearAllAssets();
+        AttackStep.clearAllAttackSteps();
     }
-	
+
 }
